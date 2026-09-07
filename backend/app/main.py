bcb59b24899391578241442d30c6e56c464c8de5
Aryan Kumar Koli <callmearyan0702@gmail.com>
2026-09-07 21:55:10 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.cors_origin_list,https://lead-forge-kwm46p1rf-jinx18.vercel.app],
+    allow_origins=[settings.cors_origin_list,"https://lead-forge-kwm46p1rf-jinx18.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
